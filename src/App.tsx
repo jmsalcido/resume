b2e20d2bfe248b2c136fba1779423ffa829078b3
Jose Salcido <jmsalcidoaguilar@gmail.com>
2025-08-01 17:19:32 -0700

    remove margin at top

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import resume from './data/resume';
 function App() {
   return (
     <div className="min-h-screen bg-gray-50">
-      <div className="max-w-6xl mx-auto mt-8 bg-white shadow-lg">
+      <div className="max-w-6xl mx-auto bg-white shadow-lg">
         <div className="flex flex-col lg:flex-row">
           <Sidebar 
             profile={developer.profile} 
