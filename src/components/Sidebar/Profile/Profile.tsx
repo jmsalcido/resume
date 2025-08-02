eea9ba4c26164dfc9d321a5f44b325f64dc0c448
Jose Salcido <jmsalcidoaguilar@gmail.com>
2025-08-01 17:56:21 -0700

    update profile

--- src/components/Sidebar/Profile/Profile.tsx
+++ src/components/Sidebar/Profile/Profile.tsx
@@ -8,7 +8,7 @@ interface ProfileProps {
 function Profile(props: ProfileProps) {
   return (
     <div className="p-8 bg-gray-900 text-center">
-      <img src='/images/developer.png' width={100} height={100} alt="logo" className="mx-auto mb-4 rounded-full" />
+      <img src='/images/developer.jpeg' width={100} height={100} alt="logo" className="mx-auto mb-4 rounded-full" />
       <h1 className="text-3xl font-bold text-white mb-2">{props.name}</h1>
       <h3 className="text-lg text-gray-300">{props.tagline}</h3>
     </div>
